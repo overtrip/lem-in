e5b95f27a609f039c744a16bf4bd78d8b4a433f6
jealonso <jealonso@student.42.fr>
2016-05-20 17:00:29 +0200

    look like good

--- srcs/print_normaly.c
+++ srcs/print_normaly.c
@@ -6,7 +6,7 @@
 /*   By: jealonso <jealonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/20 16:05:11 by jealonso          #+#    #+#             */
-/*   Updated: 2016/05/20 16:15:37 by jealonso         ###   ########.fr       */
+/*   Updated: 2016/05/20 16:59:21 by jealonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	normal_execution(t_list *map, t_room *room, t_chain *network)
 	if (network)
 	{
 		ft_putlist(map);
-	ft_putchar('\n');
+		ft_putchar('\n');
 		ft_resolver(&network, ft_atoi(map->data), room);
 	}
 }
