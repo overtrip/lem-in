c65b4578ead25b73c4d3b0bb172646f1ca08d522
jealonso <jealonso@student.42.fr>
2016-06-05 18:09:01 +0200

    correct name with a '-' inside

--- srcs/tools_research.c
+++ srcs/tools_research.c
@@ -6,7 +6,7 @@
 /*   By: jealonso <jealonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/18 16:53:25 by jealonso          #+#    #+#             */
-/*   Updated: 2016/05/26 16:52:08 by jealonso         ###   ########.fr       */
+/*   Updated: 2016/06/05 17:33:21 by jealonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,31 @@ int				exclude_comment(char *str)
 		return (1);
 }
 
+int	valide_name(char *str)
+{
+	int i;
+	int	space;
+	int	link;
+
+	i = 0;
+	space = 0;
+	link = 0;
+	while (str[i])
+	{
+		if (str[i] == ' ')
+			++space;
+		if (str[i] == '-')
+			++link;
+		if ((space > 0 && link > 0))
+		{
+			ft_putendl_fd("ERROR: Name containt a '-' character", 2);
+			return (0);
+		}
+		++i;
+	}
+	return (1);
+}
+
 int				test_possible(char *map, int *ants, int *piece)
 {
 	if (!*map)
@@ -56,10 +81,15 @@ int				test_possible(char *map, int *ants, int *piece)
 	else if (map[0] == '#')
 		return (0);
 	else if (map[0] == 'L')
+	{
+		ft_putendl_fd("ERROR: Room name by a 'L'", 2);
 		return (1);
+	}
 	else if (*ants == 1)
 	{
-		if (map_or_link(map, piece))
+		if (!valide_name(map))
+			return (1);
+		else if (map_or_link(map, piece))
 			return (1);
 	}
 	else if (nb_of_ant(map, ants))
